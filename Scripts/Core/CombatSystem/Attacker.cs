85af527ac4533daf198f8b685c869a42c6635103
Mitrofan <118289864+ZloyKorovanovich@users.noreply.github.com>
2023-03-13 18:33:49 +0300

    Add files via upload

---
+++ Scripts/Core/CombatSystem/Attacker.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Attacker
+{
+    private const string _ANIMATOR_BOOL_ATTACK = "Attack";
+
+    private Animator _animator;
+    private WeaponController _weapon;
+}
--- Scripts/Core/MovingSystem/Mover.cs
+++ Scripts/Core/MovingSystem/Mover.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Mover
 {
-    private const string _ANIMATOR_VERTICAL = "Vertical";
-    private const string _ANIMATOR_HORIZONTAL = "Horizontal";
-    private const string _ANIMATOR_STATE = "State";
-    private const string _ANIMATOR_ROTATION = "Rotation";
-    private const string _ANIMATOR_IS_ROTATING = "IsRotating";
+    private const string _ANIMATOR_FLOAT_VERTICAL = "Vertical";
+    private const string _ANIMATOR_FLOAT_HORIZONTAL = "Horizontal";
+    private const string _ANIMATOR_FLOAT_STATE = "State";
+    private const string _ANIMATOR_FLOAT_ROTATION = "Rotation";
+    private const string _ANIMATOR_BOOL_ROTATING = "Rotating";
 
     private Animator _animator;
 
@@ -32,9 +32,9 @@ public class Mover
 
     private void SetAnimatorAxisStats(Vector3 axis, float deltaTime, float sensetivity)
     {
-        _animator.SetFloat(_ANIMATOR_VERTICAL, axis.z, 1f / sensetivity, deltaTime);
-        _animator.SetFloat(_ANIMATOR_HORIZONTAL, axis.x, 1f / sensetivity, deltaTime);
-        _animator.SetFloat(_ANIMATOR_STATE, 1f, 1f / sensetivity, deltaTime);
+        _animator.SetFloat(_ANIMATOR_FLOAT_VERTICAL, axis.z, 1f / sensetivity, deltaTime);
+        _animator.SetFloat(_ANIMATOR_FLOAT_HORIZONTAL, axis.x, 1f / sensetivity, deltaTime);
+        _animator.SetFloat(_ANIMATOR_FLOAT_STATE, 1f, 1f / sensetivity, deltaTime);
     }
 
     private void SetMovement(Transform body, Vector3 target, Vector3 axis, float deltaTime, float sensetivity, float luft)
@@ -58,7 +58,7 @@ public class Mover
         body.LookAt(target);
 
         float angle = Mathf.DeltaAngle(body.eulerAngles.y, oldRotation.y);
-        _animator.SetFloat(_ANIMATOR_ROTATION, -Mathf.Sign(angle), 1f / sensetivity, deltaTime);
+        _animator.SetFloat(_ANIMATOR_FLOAT_ROTATION, -Mathf.Sign(angle), 1f / sensetivity, deltaTime);
 
         angle = Mathf.Abs(angle);
         deltaTime *= sensetivity;
@@ -78,6 +78,46 @@ public class Mover
             rotating = false;
         }
 
-        _animator.SetBool(_ANIMATOR_IS_ROTATING, rotating);
+        _animator.SetBool(_ANIMATOR_BOOL_ROTATING, rotating);
+    }
+}
+
+
+public struct MoverInputs
+{
+    private Vector3 _inputAxis;
+    private Vector3 _targetPosition;
+
+
+    public Vector3 InputAxis => _inputAxis;
+    public Vector3 TargetPosition => _targetPosition;
+
+
+    public MoverInputs(Vector3 inputAxis, Vector3 targetPosition)
+    {
+        _inputAxis = inputAxis;
+        _targetPosition = targetPosition;
+    }
+}
+
+public struct MoverOtputs
+{
+    private Vector3 _position;
+    private Vector3 _eulerAngles;
+
+    private float _movingMagnitude;
+
+
+    public Vector3 Position => _position;
+    public Vector3 EulerAngles => _eulerAngles;
+
+    public float MovingMagnitude => _movingMagnitude;
+
+
+    public MoverOtputs(Vector3 position, Vector3 eulerAngles, float movingMagnitude)
+    {
+        _position = position;
+        _eulerAngles = eulerAngles;
+        _movingMagnitude = movingMagnitude;
     }
 }
